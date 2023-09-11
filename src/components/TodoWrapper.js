6301118de7f794bba01687e6ce8423394ffe783c
Nadia-si <nasimbi2011@gmail.com>
2023-09-11 23:41:35 +0200

    Created components and todo form

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { Todo } from "./Todo";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, complete: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
+    </div>
+  );
+};
